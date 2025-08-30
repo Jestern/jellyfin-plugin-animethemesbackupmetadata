92431091ce4d8f995b3aa2725e1712c358463476
Jestern <contact.jestern@proton.me>
2025-08-30 15:08:11 +0000

    Fix actions

--- scripts/get-jellyfin-source.sh
+++ scripts/get-jellyfin-source.sh
@@ -2,9 +2,10 @@
 
 source_path=$1
 
+jellyfin_branch="master"
 jellyfin_server_git="https://github.com/jellyfin/jellyfin.git"
 jellyfin_web_git="https://github.com/jellyfin/jellyfin-web.git"
 
 mkdir -p $source_path
-git -C $source_path clone $jellyfin_server_git
-git -C $source_path clone $jellyfin_web_git
+git -C $source_path clone -b $jellyfin_branch $jellyfin_server_git
+git -C $source_path clone -b $jellyfin_branch $jellyfin_web_git

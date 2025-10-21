783f084c19320fb061f20cb631053e6bbd2b2b8e
Jestern <contact.jestern@proton.me>
2025-10-21 14:48:55 +0000

    Switch to stable jellyfin version

--- Jellyfin.Plugin.AnimeThemesBackupMetadata/Providers/ProviderNames.cs
+++ Jellyfin.Plugin.AnimeThemesBackupMetadata/Providers/ProviderNames.cs
@@ -3,4 +3,5 @@ namespace Jellyfin.Plugin.AnimeThemesBackupMetadata.Providers;
 public static class ProviderNames
 {
     public const string AnimeThemes = "AnimeThemes";
+    public const string AnimeThemesSlug = "AnimeThemesSlug";
 }
